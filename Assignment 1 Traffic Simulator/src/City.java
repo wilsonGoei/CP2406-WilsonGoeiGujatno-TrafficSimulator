6a515a6456ff8cfaa1ce6a1f0edeb697fd026c3d
wilsonGoei <wilson.gujatno@my.jcu.edu.au>
2020-01-23 19:52:41 +0800

    Create the Display of the City

--- Assignment 1 Traffic Simulator/src/City.java
+++ Assignment 1 Traffic Simulator/src/City.java
@@ -37,3 +37,80 @@ public class City extends JPanel {
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
     }
+
+    /**The display of the city**/
+    public void paint(Graphics graphics) {
+
+        super.paintComponent(graphics);
+
+
+        graphics.setColor(darkPink); // color of the road
+        graphics.fillRect(350, 0, 50, 600); // size and position of the road
+
+        graphics.setColor(Color.WHITE); // color of the stripes
+        for(int a = roadHeight; a < roadHeight *4; a = a + roadHeight) {
+            for(int b=0;b<getHeight();b+=40) {
+                graphics.fillRect(375, b, 5, 30); // size and position of the stripes
+            }
+        }
+
+
+
+
+        graphics.setColor(darkPink); // color of the road
+        graphics.fillRect(0, 100, 870, 50); // size and position of the road
+
+        graphics.setColor(Color.WHITE); // color of the stripes
+        for(int a = roadHeight; a< roadHeight *4; a=a+ roadHeight) {
+            for(int b=0;b<getWidth();b+=40) {
+                graphics.fillRect(b, 120, 30, 5); // size and position of the road
+            }
+        }
+
+
+
+        graphics.setColor(darkPink); // color of the road
+        graphics.fillRect(0, 400, 870, 50); // size and position of the road
+
+        graphics.setColor(Color.WHITE); // color of the stripes
+        for(int a = roadHeight; a< roadHeight *4; a=a+ roadHeight) {
+            for(int b=0;b<getWidth();b+=40) {
+                graphics.fillRect(b, 420, 30, 5); // size and position of the road
+            }
+        }
+
+
+        graphics.setColor(darkPink); // color of the road
+        graphics.fillRect(150, 400, 50, 600); // size and position of the road
+
+        graphics.setColor(Color.WHITE); // color of the stripes
+        for(int a = roadHeight; a< roadHeight *4; a=a+ roadHeight) {
+            for(int b=430;b<getHeight();b+=40) {
+                graphics.fillRect(173, b, 5, 30); // size and position of the road
+            }
+        }
+
+
+
+        graphics.setColor(darkPink); // color of the road
+        graphics.fillRect(650, 400, 50, 600); // size and position of the road
+
+        graphics.setColor(Color.WHITE); // color of the stripes
+        for(int a = roadHeight; a< roadHeight *4; a=a+ roadHeight) {
+            for(int b=430;b<getHeight();b+=40) {
+                graphics.fillRect(673, b, 5, 30); // size and position of the stripes
+            }
+        }
+
+
+
+
+        for(int a = 0; a< vehicles.size(); a++) {
+            vehicles.get(a).paintMe(graphics); // display the vehicles
+        }
+        for(int a = 0; a< trafficLights.size(); a++) {
+            trafficLights.get(a).paintMe(graphics); // display the traffic lights
+        }
+
+    }
+
