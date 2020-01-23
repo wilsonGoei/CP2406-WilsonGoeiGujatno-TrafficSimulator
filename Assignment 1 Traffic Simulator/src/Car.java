cacea0d75ef923e9e7b4fe139ac94e19e5e51c28
wilsonGoei <wilson.gujatno@my.jcu.edu.au>
2020-01-23 19:41:08 +0800

    Edit car class

--- Assignment 1 Traffic Simulator/src/Car.java
+++ Assignment 1 Traffic Simulator/src/Car.java
@@ -1,18 +1,28 @@
-public class Car {
+import java.awt.Color;
+import java.awt.Graphics;
 
-    String name;
-    int position;
-    Road current_car_position;
+public class Car extends Vehicle{
 
-    public Car(String name, int position, Road current_car_position) {
+    private Color tosca = new Color(102,255,255); // the rgb color for the bus
 
-        this.name = name;
-        this.position = position;
-        this.current_car_position = current_car_position;
+    /**Instantiate the Car class**/
+    public Car(int x_axis, int y_axis) {
+        super(x_axis, y_axis); // x_axis and y_axis from the vehicle class
+        vehicle_speed = 15; // set the speed of the car
+        vehicle_height = 10; // set the height of the car
+        vehicle_width = 30; // set the width of the car
     }
 
-    public void movement() {
-
-        this.position += 1;
+    /**Control the Car display**/
+    public void paintMe(Graphics graphics) {
+        graphics.setColor(tosca); // set the color of the car
+        graphics.fillRect(x_axis, y_axis, vehicle_width, vehicle_height); // set the position and the size of the car
     }
+
+    /**Set the speed of the Car**/
+    public void setRate(int r) {
+        this.vehicle_speed =r;
+    }
+
+
 }
